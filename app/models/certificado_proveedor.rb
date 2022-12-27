d8404d131b95feb625faa604718bea6fbb9a87ee
guillermo varela <guillovarela16@gmail.com>
2022-12-27 11:13:00 -0300

    Change add y remove items in certificado forms

--- app/models/certificado_proveedor.rb
+++ app/models/certificado_proveedor.rb
@@ -2,4 +2,6 @@ class CertificadoProveedor < ApplicationRecord
   belongs_to :registro_proveedor
   belongs_to :materia_sustancia
   belongs_to :actividad_economica
+
+  mount_uploader :archivo_certificado, ArchivoCertificadoProveedorUploader
 end
