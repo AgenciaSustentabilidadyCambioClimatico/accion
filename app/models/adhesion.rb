178283e3c0391b08e211e997f161d73980cfe083
cazambraL <cazambra99@gmail.com>
2023-07-25 22:46:14 -0400

    [AI-115] Avance descargables adhesiones

--- app/models/adhesion.rb
+++ app/models/adhesion.rb
@@ -523,6 +523,7 @@ class Adhesion < ApplicationRecord
 			self.adhesion_elemento_retirados.map{|a| 
 				fila = a.fila
 				fila[:documento_justificacion] = a.archivo_retiro
+				fila[:adhesion_externa_id] = self.id
 				fila
 			}
 		else
