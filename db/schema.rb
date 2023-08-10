d625c25f00a485635e2e95acfa4ab5c0363be045
guillermo varela <guillovarela16@gmail.com>
2023-08-10 12:58:37 -0300

    Change link in historial de instrumentos

--- db/schema.rb
+++ db/schema.rb
@@ -1241,9 +1241,10 @@ ActiveRecord::Schema.define(version: 20230809181130) do
   create_table "nota_registro_proveedores", force: :cascade do |t|
     t.bigint "registro_proveedor_id"
     t.bigint "manifestacion_de_interes_id"
-    t.integer "nota", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "calificado", default: false
+    t.float "nota", default: 0.0
     t.index ["manifestacion_de_interes_id"], name: "index_nota_registro_proveedores_on_manifestacion_de_interes_id"
     t.index ["registro_proveedor_id"], name: "index_nota_registro_proveedores_on_registro_proveedor_id"
   end
@@ -1618,9 +1619,9 @@ ActiveRecord::Schema.define(version: 20230809181130) do
     t.date "fecha_aprobado"
     t.date "fecha_revalidacion"
     t.string "archivo_aprobado_directiva"
-    t.string "carta_compromiso"
     t.string "comentario_negativo"
     t.boolean "calificado", default: false
+    t.string "carta_compromiso"
     t.index ["contribuyente_id"], name: "index_registro_proveedores_on_contribuyente_id"
     t.index ["tipo_contribuyente_id"], name: "index_registro_proveedores_on_tipo_contribuyente_id"
     t.index ["tipo_proveedor_id"], name: "index_registro_proveedores_on_tipo_proveedor_id"
@@ -2010,6 +2011,7 @@ ActiveRecord::Schema.define(version: 20230809181130) do
   add_foreign_key "flujo_tareas", "tareas", column: "tarea_entrada_id"
   add_foreign_key "flujo_tareas", "tareas", column: "tarea_salida_id"
   add_foreign_key "flujos", "contribuyentes"
+  add_foreign_key "flujos", "manifestacion_de_intereses"
   add_foreign_key "flujos", "manifestacion_de_intereses", name: "flujos_manifestacion_de_interes_id_fkey"
   add_foreign_key "flujos", "programa_proyecto_propuestas"
   add_foreign_key "flujos", "proyectos"
