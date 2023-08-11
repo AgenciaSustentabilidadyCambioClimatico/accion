0ba0b721d6aafc2d0c930fa7a5460e6d7f0e67b4
guillermo varela <guillovarela16@gmail.com>
2023-08-11 11:05:00 -0300

    Fix solo lectura in actores form

--- app/controllers/actores_controller.rb
+++ app/controllers/actores_controller.rb
@@ -209,6 +209,7 @@ class ActoresController < ApplicationController
 
   #DZC define el flujo y tipo_instrumento, junto con la manifestación o el proyecto según corresponda, para efecto de completar datos. El id de la manifestación se obtiene del flujo correspondiente a la tarea pendiente.
   def set_flujo
+    @solo_lectura = params[:q]
     @flujo = @tarea_pendiente.flujo
     @tarea = @tarea_pendiente.tarea
     @tipo_instrumento=@flujo.tipo_instrumento
@@ -233,7 +234,6 @@ class ActoresController < ApplicationController
   end
 
   def set_mapa_de_actores_data
-    @solo_lectura = params[:q]
     if action_name.to_sym == :actualizacion ||action_name.to_sym == :actualizar
       accion = :actualizacion
     elsif action_name.to_sym == :revision ||action_name.to_sym == :enviar_revision
