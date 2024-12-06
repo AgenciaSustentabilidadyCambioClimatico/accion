a081910664fb652503295b9fcac74233133d2ca2
guillermo varela <guillovarela16@gmail.com>
2024-12-06 11:31:07 -0300

    Remove line from mailer aplication

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,7 +6,6 @@ class ApplicationMailer < ActionMailer::Base
   def interceptor
     if %w[stag dev].include?(Rails.configuration.ambiente)
       Rails.logger.info "Modifying email recipient for environment: #{Rails.configuration.ambiente}"
-
       original_recipient = message.to.try(:first) || "unknown"
       message.subject = "#{Rails.configuration.ambiente.upcase}: (#{original_recipient}) #{message.subject}"
       message.to = "sistemas@ascc.cl"
