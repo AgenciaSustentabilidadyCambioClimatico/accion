94139b28e59d384e56764538e9d6ca19eac01f88
guillermo varela <guillovarela16@gmail.com>
2024-12-10 15:08:35 -0300

    Change email receiver

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,7 +8,7 @@ class ApplicationMailer < ActionMailer::Base
     if %w[stag dev].include?(ENV["AMBIENTE"])
       logger.info { "Modificando destinatario email..." }
       message.subject = "#{ENV["AMBIENTE"]}: (#{message.try(:to).try(:first).to_s}) #{message.subject}"
-      message.to = "sistemaacion@ascc.cl"
+      message.to = "dev@ascc.cl"
     end
   end
 end
