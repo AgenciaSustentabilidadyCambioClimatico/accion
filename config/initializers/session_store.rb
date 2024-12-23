2f060a45343d7b5bc1d607d04265a6a0865d7a3c
guillermo varela <guillovarela16@gmail.com>
2024-12-23 08:29:50 -0300

    Add new session store code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,1 @@
-if %w[prod].include?(ENV['AMBIENTE'])
-  Rails.application.config.session_store :cookie_store, key: ENV['SECRET_KEY_BASE'], domain: 'accion.ascc.cl'
-elsif %w[stag].include?(ENV['AMBIENTE'])
-  Rails.application.config.session_store :cookie_store, key: ENV['SECRET_KEY_BASE'], domain: 'accion-stag.ascc.cl'
-else
-  Rails.application.config.session_store :cookie_store, key: ENV['SECRET_KEY_BASE'], domain: :all
-end
+Rails.application.config.session_store :cookie_store, key: ENV['SECRET_KEY_BASE'], domain: :all
