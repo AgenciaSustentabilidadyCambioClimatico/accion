d52ec2c94357275501c80bd942fc32ea9af26232
guillermo varela <guillovarela16@gmail.com>
2024-12-30 13:36:24 -0300

    Merge develop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20241230143715) do
+ActiveRecord::Schema.define(version: 2024_12_30_143715) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
