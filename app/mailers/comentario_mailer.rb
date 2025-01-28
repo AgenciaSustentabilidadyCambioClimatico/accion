dc004804388336a52eade4cd0b4e751e56756d9d
jvillena <villenasoft@gmail.com>
2025-01-28 17:25:12 -0300

    se implementa descarga del formulario fpl en la tarea FPL-01 3

--- app/mailers/comentario_mailer.rb
+++ app/mailers/comentario_mailer.rb
@@ -2,12 +2,12 @@ class ComentarioMailer < ApplicationMailer
 
 	def nuevo(comentario)
     @comentario = comentario
-    mail(to: comentario.email_contacto, subject: 'Nuevo comentario del sitio ASCC')
+    mail(to: comentario.email_contacto, subject: 'Nuevo comentario del sitio ASCC ID:' + @comentario.id.to_s)
   end
 
   def nuevo_para_revisor(comentario, user)
     @comentario = comentario
-    mail(to: user.email_institucional, subject: 'Nuevo comentario del sitio ASCC')
+    mail(to: user.email_institucional, subject: 'Nuevo comentario del sitio ASCC ID:' + @comentario.id.to_s)
   end
 
   def welcome(recipient)
