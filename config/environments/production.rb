a356ad50af8d747763ed32454a73ca6f408f1607
Cristobal Zambrano <62403904+cazambraL@users.noreply.github.com>
2025-07-15 15:43:01 -0400

    se elimina error midleware en stag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,13 +109,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   #oob_uri for Google Calendar authentication
   config.oob_uri = 'https://accion.ascc.cl/oauth2callback'
-  
-  # Configurar límites para archivos grandes en Rack
-  config.middleware.insert_before Rack::Runtime, Rack::Utils
-  config.middleware.use Rack::Utils do |env|
-    env['rack.input'] = env['rack.input'].tap do |input|
-      input.instance_variable_set(:@key_space_limit, 50 * 1024 * 1024) # 50MB
-      input.instance_variable_set(:@param_depth_limit, 100)
-    end
-  end
 end
