621789d4027c54a595681ef916679fc01b62ec25
villenasoft <villenasoft@gmail.com>
2026-06-18 10:31:01 -0400

    se implementa test de envio de correos para verificar el sistema de envíos 2

--- app/controllers/admin/test_correos_controller.rb
+++ app/controllers/admin/test_correos_controller.rb
@@ -1,13 +1,18 @@
 class Admin::TestCorreosController < ApplicationController
-  before_action :authenticate_user! # Asegura que solo admins ejecuten esto
+  before_action :authenticate_user!
 
   def create
     asunto = "TEST: Verificación del sistema de envíos"
     cuerpo = "Este es un correo de prueba generado desde el panel administrativo."
  
-    TestMailer.notificacion_test_mail(asunto, cuerpo).deliver_later
+    begin
+      TestMailer.notificacion_test_mail(asunto, cuerpo).deliver_now
+      flash[:notice] = "Test enviado correctamente a los administradores."
+    rescue => e
+      flash[:alert] = "No se pudo enviar el correo de prueba: #{e.message}"
+      Rails.logger.error("Error en TestMailer: #{e.message}")
+    end
 
-    flash[:notice] = "Test enviado a administradores."
     redirect_to request.referrer
   end
 end
